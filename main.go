a4e6d156d6ed5a6ad7cb16dee834f88f43cd105f
bbr <bj@webedia.fr>
2017-07-31 13:51:21 +0200

    add token resolution

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
+	"html/template"
+	"net/http"
 	"spotapi"
 )
 
 func main() {
-	api := spotapi.LoadClient()
-	fmt.Println(api.GetUrlAuth())
+	client := spotapi.LoadClient()
+
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		t, _ := template.ParseFiles("public/auth.html")
+		t.Execute(w, client)
+	})
+
+	http.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		code := r.FormValue("code")
+		w.Write([]byte("hello, your code is now:" + code))
+	})
+
+	http.ListenAndServe(":8686", nil)
 }
